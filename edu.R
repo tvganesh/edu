e066aaceb5e964030aada17609950fe1d3b2e66a
Tinniam V Ganesh <tvganesh.85@gmail.com>
2015-10-24 21:08:06 +0530

    Changes

--- edu.R
+++ edu.R
@@ -3,9 +3,9 @@ library(maptools)
 library(ggplot2)
 library(dplyr)
 library(stringr)
-#a <- read.csv("education.csv")
+b <- read.csv("education.csv")
 #colnames(a) <- gsub("Educational.level...","",colnames(a))
-a <- read.csv("india.csv")
+#a <- read.csv("india.csv")
 #write.csv(b,file="india.csv")
 b <- filter(a,Area.Name=="INDIA" & Total..Rural..Urban=="Total")
 # Create a vector
@@ -37,7 +37,7 @@ m1 <- males[2:23,v]
 m2 <- melt(m1,id.vars="age")
 ggplot(m2,aes(x=age,y=value,fill=variable)) + geom_bar(stat = "identity")
 
-
+#######################################################################
 b <- filter(a,Area.Name=="INDIA" & Total..Rural..Urban=="Total")
 # Select colums from 8 - 21
 c <- b[,8:21]
@@ -82,6 +82,7 @@ lines(m,d$Population.attending.educational.institutions...Persons)
 
 barplot(d$Population.attending.educational.institutions...Persons,names.arg=d$Age.group,col=colors)
 
+########################################################
 ind_dist <- readShapeSpatial("./IND_adm/IND_adm2.shp")
 #ind <- fortify(ind_dist, region = "NAME_1")
 
@@ -136,3 +137,90 @@ ggplot() + geom_map(data = c, aes(map_id = Area.Name, fill = tot),
                     map = dist) + expand_limits(x = dist$long, y = dist$lat) + 
     scale_fill_gradient2(low = "grey",                                                                           
                          mid = "light blue", midpoint = mid, high = "blue", limits = c(n, m))
+
+
+##################################################################################
+c <- filter(b,Age.group=="All ages" & Total..Rural..Urban=="Total")
+
+l <- c[,5:21]
+#Calculate percentages
+
+m <- l[,7:15]/l[,4]*100
+
+#Bind the State column
+m <- cbind(l$Area.Name,m)
+
+# Fix column names
+names(m) <-c("Area.Name","PersonsEdu","MalesEdu",
+             "FemalesEdu","IlliteratePersons","IlliterateMales",
+             "IlliterateFemales","LiteratePersons","LiterateMales","LiterateFemales")
+
+
+# Remove the row corresponding to India
+m <- m[2:length(rownames(m)),]
+m$Area.Name <-gsub("State - ","",m$Area.Name)
+m$Area.Name <- gsub("\\d+","",m$Area.Name)
+
+# Remove trailing spaces
+m$Area.Name <- gsub("[[:space:]]*$","",m$Area.Name)
+
+ind <- readShapeSpatial("./India_SHP/INDIA.shp")
+plot(ind)
+
+ind <- fortify(ind, region = "ST_NAME")
+
+
+# Set the names as in the map
+m[m$Area.Name=="JAMMU & KASHMIR",]$Area.Name = "Jammu And Kashmir"
+
+m[m$Area.Name=="HIMACHAL PRADESH",]$Area.Name = "Himachal Pradesh"
+m[m$Area.Name=="PUNJAB",]$Area.Name = "Punjab"
+m[m$Area.Name=='UTTARANCHAL',]$Area.Name = "Uttarakhand"
+m[m$Area.Name=="CHANDIGARH",]$Area.Name = "CHANDIGARH"
+
+m[m$Area.Name=="HARYANA",]$Area.Name = "Haryana"
+
+m[m$Area.Name=="DELHI",]$Area.Name = "Nct Of Delhi"
+m[m$Area.Name=="RAJASTHAN",]$Area.Name = "Rajasthan"
+m[m$Area.Name=="UTTAR PRADESH",]$Area.Name = "Uttar Pradesh"
+m[m$Area.Name=="BIHAR",]$Area.Name = "Bihar"
+m[m$Area.Name=="SIKKIM",]$Area.Name = "Sikkim"
+
+m[m$Area.Name=="ARUNACHAL PRADESH",]$Area.Name = "Arunachal Pradesh"
+m[m$Area.Name=="NAGALAND",]$Area.Name = "Nagaland"
+m[m$Area.Name=="MANIPUR",]$Area.Name = "Manipur"
+m[m$Area.Name=="MIZORAM",]$Area.Name = "Mizoram"
+
+m[m$Area.Name=="TRIPURA",]$Area.Name = "Tripura"
+m[m$Area.Name=="MEGHALAYA",]$Area.Name = "Meghalaya"
+m[m$Area.Name=="ASSAM",]$Area.Name = "Assam"
+m[m$Area.Name=="WEST BENGAL",]$Area.Name = "West Bengal"
+m[m$Area.Name=="JHARKHAND",]$Area.Name = "Jharkhand"
+
+m[m$Area.Name=="ORISSA",]$Area.Name = "Orissa"
+m[m$Area.Name=="JHARKHAND",]$Area.Name = "Jharkhand"
+m[m$Area.Name=="CHHATTISGARH",]$Area.Name = "Chhattisgarh"
+m[m$Area.Name=="MADHYA PRADESH",]$Area.Name = "Madhya Pradesh"
+
+m[m$Area.Name=="GUJARAT",]$Area.Name = "Gujarat"
+m[m$Area.Name=="DAMAN & DIU",]$Area.Name = "DAMAN AND DIU"
+m[m$Area.Name=="DADRA & NAGAR HAVELI",]$Area.Name = "DADRA AND NAGAR HAVELI"
+
+m[m$Area.Name=="MAHARASHTRA",]$Area.Name = "Maharashtra"
+m[m$Area.Name=="ANDHRA PRADESH",]$Area.Name = "Andhra Pradesh"
+m[m$Area.Name=="KARNATAKA",]$Area.Name = "Karnataka"
+m[m$Area.Name=="GOA",]$Area.Name = "Goa"
+
+m[m$Area.Name=="LAKSHADWEEP",]$Area.Name = "LAKSHADWEEP"
+m[m$Area.Name=="KERALA",]$Area.Name = "Kerala"
+m[m$Area.Name=="TAMIL NADU",]$Area.Name = "Tamil Nadu"
+m[m$Area.Name=="PONDICHERRY",]$Area.Name = "Pondicherry"
+m[m$Area.Name=="ANDAMAN & NICOBAR ISLANDS",]$Area.Name = "ANDAMAN AND NICOBAR ISLANDS"
+
+
+setdiff(m$Area.Name,unique(ind$id))
+
+d <- filter(b,Age.group=="All ages" & Total..Rural..Urban=="Urban")
+e <- filter(b,Age.group=="All ages" & Total..Rural..Urban=="Rural")
+
+
